0bb018e85ae87aaa8c1a58c40140b2d422f465d5
我 <apple@appletekimacbook-air.local>
2016-12-07 19:11:22 +0800

    初始化项目

---
+++ githubTest/githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by wangbin on 16/12/7.
+//  Copyright © 2016年 kayakwise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
